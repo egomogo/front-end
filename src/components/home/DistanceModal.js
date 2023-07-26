a16cbae9d91c7ed5273db8e2578a7c292f730094
한별 <78421872+2hanbyeol1@users.noreply.github.com>
2023-07-26 21:11:22 +0900

    [FEAT] 거리 모달 확인 버튼 추가 (#15)
    * [FEAT] 거리 모달 확인 버튼 추가
    
    * [FIX] 슬라이더에 따라 반경 표시

--- src/components/home/DistanceModal.js
+++ src/components/home/DistanceModal.js
@@ -3,19 +3,20 @@ import Container from '../common/Container';
 import KakaoMap from '../common/KakaoMap';
 import Modal from '../common/Modal';
 import CustomSlider from '../common/Slider';
+import TextButton from '../common/TextButton';
 import { SliderColor } from '../../constants/Color';
+import { useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { minuteState } from '../../atom';
 
-const DistanceModal = ({
-  currentCoord,
-  visible,
-  onRequestClose,
-  minute,
-  onSlidingComplete,
-}) => {
+const DistanceModal = ({ currentCoord, visible, onRequestClose }) => {
+  const [minute, setMinute] = useRecoilState(minuteState);
+  const [slider, setSlider] = useState(minute);
+
   return (
     <Modal visible={visible} onRequestClose={onRequestClose}>
       <Container>
-        <KakaoMap currentCoord={currentCoord} minute={minute} />
+        <KakaoMap currentCoord={currentCoord} minute={slider} />
         <Text>추천 식당까지 거리 설정하기</Text>
         <CustomSlider
           width={300}
@@ -26,8 +27,18 @@ const DistanceModal = ({
           showValue
           text="분"
           mainColor={SliderColor.mainRed}
-          onSlidingComplete={onSlidingComplete}
+          onSlidingComplete={(value) => {
+            setSlider(value);
+          }}
         />
+        <TextButton
+          onPress={() => {
+            setMinute(slider);
+            onRequestClose();
+          }}
+        >
+          확인
+        </TextButton>
       </Container>
     </Modal>
   );
