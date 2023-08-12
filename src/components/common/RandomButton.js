132d24ec43a538c07603788c03ac0d45796fb6b6
heeeione <heewon410@naver.com>
2023-08-12 16:25:57 +0900

    [Fix] bottom icon 고침

--- src/components/common/RandomButton.js
+++ src/components/common/RandomButton.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { TouchableOpacity, Text, Image, StyleSheet, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useState } from 'react';
