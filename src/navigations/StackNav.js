abc72fef5496e1e036737a7eff4116924e328889
heeeione <heewon410@naver.com>
2023-08-15 18:25:29 +0900

    [Feat] 홈 화면 빼고 하단 바 지우기

--- src/navigations/StackNav.js
+++ src/navigations/StackNav.js
@@ -2,10 +2,21 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../screens/Home';
 import RandomBox from '../screens/RandomBox';
 import MoreView from '../screens/MoreView';
+import { useLayoutEffect } from 'react';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 
 const Stack = createStackNavigator();
 
-const RandomStack = () => {
+const RandomStack = ({ navigation, route }) => {
+  useLayoutEffect(() => {
+    const routeName = getFocusedRouteNameFromRoute(route);
+    if (routeName === 'Home' || routeName === undefined) {
+      navigation.setOptions({ tabBarStyle: { display: 'flex' } });
+    } else {
+      navigation.setOptions({ tabBarStyle: { display: 'none' } });
+    }
+  }, [navigation, route]);
+
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Home" component={Home} />
