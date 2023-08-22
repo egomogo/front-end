89171ce4fefd0394da03f289a094a49cefb3800b
최선정 <128480236+Choeseonjeong@users.noreply.github.com>
2023-08-22 16:04:57 +0900

    [FEAT]Update wishlist.js

--- src/axios/wishlist.js
+++ src/axios/wishlist.js
@@ -4,11 +4,8 @@ export const fetchWishList = async (ids) => {
   try {
     const idString = ids.join(',');
     const url = `http://13.125.224.220:8080/api/v1/restaurants/wishes?ids=${idString}`;
-    console.log("Requesting URL: ", url);
 
     const response = await request.get(url);
-
-    console.log("API Response: ", response.data);
 
     if (response.status !== 200) {
       console.error("Response Status Code: ", response.status);
